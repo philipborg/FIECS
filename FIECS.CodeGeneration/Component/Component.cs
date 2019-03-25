15480e62698041e5e4257e79bffd83c04664b3e8
philipborg <philip.borgstrom@gmail.com>
2019-03-25 21:03:51 +0100

    Remove FIECS.Attributes

---
+++ FIECS.CodeGeneration/Component/Component.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace FIECS.Attributes.Component
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct, AllowMultiple = false, Inherited = false)]
+    [Conditional("CodeGeneration")]
+    public class Component : Attribute
+    {
+    }
+}
