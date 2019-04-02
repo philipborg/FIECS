98e5a15066fb6bb6f34c30d292df635eea99c455
philipborg <philip.borgstrom@gmail.com>
2019-04-02 21:29:40 +0200

    Buildable solution

---
+++ FIECS.Compile.Fody/ModuleWeaver.cs
@@ -0,0 +1,19 @@
+﻿using Fody;
+using System.Collections.Generic;
+
+namespace FIECS.Compile.Fody
+{
+    public class ModuleWeaver : BaseModuleWeaver
+    {
+        public override void Execute()
+        {
+        }
+
+        public override IEnumerable<string> GetAssembliesForScanning()
+        {
+            yield break;
+        }
+
+        public override bool ShouldCleanReference => false;
+    }
+}
